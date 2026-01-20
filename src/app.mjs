37154ddf12bc02717599e5d70cbeed227b2477af
Bhavyagautam08 <bhavyagautam0608@gmail.com>
2026-01-20 17:08:29 +0530

    Fix module imports, add jsonwebtoken and cors

--- src/app.mjs
+++ src/app.mjs
@@ -1,17 +1,19 @@
-import express from "express" 
-import dotenv from "dotenv" 
+import express from "express"
+import dotenv from "dotenv"
 import router from "./routes/expenseRoutes.mjs"
-import "./jobs/cronJob"
+import "./jobs/cronJob.mjs"
+import cors from "cors"
 
-dotenv.config() 
+dotenv.config()
 
-const app = express() ;
+const app = express();
+app.use(cors());
 
-app.use(express.json()) ;
-app.use("/api/expenses" , router) ;
+app.use(express.json());
+app.use("/api/expenses", router);
 
-const PORT = process.env.PORT || 4000 ;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT , () =>{
-    console.log(`Server is live on ${PORT}`) ;
+app.listen(PORT, () => {
+    console.log(`Server is live on ${PORT}`);
 })
